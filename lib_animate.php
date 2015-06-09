1b177f228ddd1e43b4422995332956e19ad70afe
Messerbill <ole.hoppe@gmx.de>
2015-06-09 14:11:10 +0200

    added lib_animate

---
+++ lib_animate.php
@@ -0,0 +1,10 @@
+<?php
+namespace LIB;
+class lib_animate extends \LIB\lib_css{
+    public static function get_class(){
+        return self::class;}
+    public static function css_path(){
+        return \SYSTEM\WEBPATH(new \SYSTEM\PLIB(),'tablesorter/lib/animate.min.css');}
+    public static function version(){
+        return '<a href="https://github.com/daneden/animate.css" target="_blank">https://github.com/daneden/animate.css</a> (commit: 9d4c59e5e5)';}
+}
