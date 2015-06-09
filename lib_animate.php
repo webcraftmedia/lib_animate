666544655bf5365e4f77aec0fa0f497673f6e69a
Messerbill <ole.hoppe@gmx.de>
2015-06-09 22:18:29 +0200

    pathfix

--- lib_animate.php
+++ lib_animate.php
@@ -4,7 +4,7 @@ class lib_animate extends \LIB\lib_css{
     public static function get_class(){
         return self::class;}
     public static function css_path(){
-        return \SYSTEM\WEBPATH(new \SYSTEM\PLIB(),'tablesorter/lib/animate.min.css');}
+        return \SYSTEM\WEBPATH(new \SYSTEM\PLIB(),'animate/lib/animate.min.css');}
     public static function version(){
         return '<a href="https://github.com/daneden/animate.css" target="_blank">https://github.com/daneden/animate.css</a> (commit: 9d4c59e5e5)';}
 }
